712c00cc708351d76f6007bf0327eea8f216d17c
Jeffrey Friel <jeffreyfriel@Jeffreys-MacBook-Pro.local>
2017-07-20 16:12:02 -0500

    Changed color dictionary to color struct, added navigation to color view

---
+++ Colors/Colors/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Colors
+//
+//  Created by Jeffrey Friel on 7/20/17.
+//  Copyright © 2017 Jeffrey Friel. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
--- Colors/Colors/ColorController.swift
+++ Colors/Colors/ColorController.swift
@@ -10,28 +10,25 @@ import UIKit
 
 class ColorController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    let colorDictKey = ["red", "orange", "yellow", "green", "blue", "purple", "brown"]
+    //let colorDictKey = ["red", "orange", "yellow", "green", "blue", "purple", "brown"]
     
-    let colorDict: [String: UIColor] = ["red": UIColor.red, "orange": UIColor.orange, "yellow": UIColor.yellow, "green": UIColor.green, "blue": UIColor.blue, "purple": UIColor.purple, "brown": UIColor.brown]
+    var colorDict = [Color(name: "red", uiColor: UIColor.red),
+                     Color(name: "orange", uiColor: UIColor.orange),
+                     Color(name: "yellow", uiColor: UIColor.yellow),
+                     Color(name: "green", uiColor: UIColor.green),
+                     Color(name: "blue", uiColor: UIColor.blue),
+                     Color(name: "purple", uiColor: UIColor.purple),
+                     Color(name: "brown", uiColor: UIColor.brown)
+                     ]
     
     
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return (colorDictKey.count)
-    }
     
-    
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
-        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
-        
-        cell.textLabel?.text = colorDictKey[indexPath.row]
-        cell.contentView.backgroundColor = colorDict[colorDictKey[indexPath.row]]
-        return cell
-    }
+    @IBOutlet weak var colorsTableView: UITableView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Colors"
         // Do any additional setup after loading the view, typically from a nib.
         
         
@@ -42,5 +39,28 @@ class ColorController: UIViewController, UITableViewDataSource, UITableViewDeleg
         // Dispose of any resources that can be recreated.
     }
     
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return (colorDict.count)
+    }
     
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = colorDict[indexPath.row].name
+        cell.backgroundColor = colorDict[indexPath.row].uiColor
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? ColorDetailViewController,
+           let row = colorsTableView.indexPathForSelectedRow?.row
+        {
+            destination.color = colorDict[row]
+        }
+    }
 }
